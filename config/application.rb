ac0811ab18c62feebff45e1ec05b42604509d49a
James Martinez <jamescmartinez@gmail.com>
2014-05-05 15:20:00 -0700

    configured generators

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module Snowball
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec, view_specs: false, request_specs: false, routing_specs: false
+    end
   end
 end
