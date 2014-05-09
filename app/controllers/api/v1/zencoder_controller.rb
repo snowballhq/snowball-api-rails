12503169a9520438146e314483de85d0784fe283
James Martinez <jamescmartinez@gmail.com>
2014-05-09 15:55:33 -0700

    zencoder controller now posts correctly

--- app/controllers/api/v1/zencoder_controller.rb
+++ app/controllers/api/v1/zencoder_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ZencoderController < ApplicationController
+class Api::V1::ZencoderController < Api::V1::ApiController
   def create
     job_id = job_params[:id].to_i
     clip = Clip.where(zencoder_job_id: job_id).first
