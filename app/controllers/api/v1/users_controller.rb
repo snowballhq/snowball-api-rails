d5945a9d1d325cbfb628bd0ae530118cc8888c44
James Martinez <jamescmartinez@gmail.com>
2014-06-13 13:21:19 -0700

    users can now be updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,22 @@
 class Api::V1::UsersController < Api::V1::ApiController
   before_action :restrict_access!
-  before_action :set_user, only: [:show]
+  before_action :set_user
 
   def show
+  end
+
+  def update
+    @user.update!(user_params)
+    render :show, status: :ok, location: api_v1_user_url(@user)
   end
 
   private
 
   def set_user
     @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password, :email, :bio)
   end
 end
