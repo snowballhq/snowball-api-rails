07e3e9942529de4418875d8862899a03c275ab9b
James Martinez <jamescmartinez@gmail.com>
2014-07-24 19:49:00 -0700

    fixed a whoops

--- app/models/reel.rb
+++ app/models/reel.rb
@@ -6,7 +6,6 @@ class Reel < ActiveRecord::Base
   has_many :participants, through: :participations, source: :user
 
   def recent_participants_names
-    return name if name
     participants.last(5).map(&:name).map do |n|
       n.split[0...-1].join ' '
     end.join(', ')
