4fc60a13ef911d12e7c8c15d33c5a82a144a81e4
James Martinez <jamescmartinez@gmail.com>
2014-07-27 14:35:54 -0700

    added todo with push notification action code

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -12,12 +12,13 @@ class Notification < ActiveRecord::Base
   def send_push_notification
     return if Rails.env.test?
     # TODO: schedule this for after the clip is saved with no zencoder ID
-    # TODO: send push actions (inside notification object) if it is a reel: :actions=>{:open=>{:type=>"deep_link", :content=>"snowball://reel/12345"}}}
     push = {
       audience: { alias: [user.id] },
       notification: { alert: message },
       device_types: "all"
     }
+    # TODO: send push actions (inside notification object) if it is a reel: :actions=>{:open=>{:type=>"deep_link", :content=>"snowball://reel/12345"}}}
+    # push[:notification].merge(actions: { open: { type: 'deep_link', content: "snowball://reel/#{notifiable.reel.id}" } }) if notifiable.type == 'Clip'
     Urbanairship.push(push.merge(version: 3))
   end
 
