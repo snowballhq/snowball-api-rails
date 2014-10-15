a68a357acc0c19ccbbc428edbf70e76c27fae233
James Martinez <jamescmartinez@gmail.com>
2014-10-14 23:05:05 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+ruby '2.1.2'
+
+gem 'rails', '4.2.0.beta2'
+gem 'pg'
+gem 'jbuilder'
+gem 'unicorn'
+
+group :development, :test do
+  gem 'spring'
+  gem 'rspec-rails'
+  gem 'shoulda'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'rubocop'
+  gem 'guard-rspec'
+  gem 'dotenv-rails'
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,26 @@
+if ENV['UNICORN_COUNT'].to_i > 0
+  worker_processes ENV['UNICORN_COUNT'].to_i
+else
+  worker_processes 4
+end
+timeout 120
+preload_app true
+
+before_fork do |_server, _worker|
+  Signal.trap 'TERM' do
+    logger.info 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |_server, _worker|
+  Signal.trap 'TERM' do
+    logger.info 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
