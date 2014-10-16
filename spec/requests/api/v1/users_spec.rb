7e8353cae40c3ca476d3cd2f9a1718ebf66e0fd7
James Martinez <jamescmartinez@gmail.com>
2014-10-16 02:32:09 -0700

    rubocop rspec remove describe class cop

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :request do
+RSpec.describe 'Users', type: :request do
   describe 'GET /users?phone_number=' do
     it 'returns users with specified phone number(s)' do
       user = create(:user)
