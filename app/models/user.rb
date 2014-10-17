56e6900e99d695f3b2858d5d27e23e27a696af92
James Martinez <jamescmartinez@gmail.com>
2014-10-16 22:55:43 -0700

    renamed gen auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
   validates :auth_token, presence: true
 
   before_validation(on: :create) do
-    generate_and_set_auth_token
+    generate_auth_token
   end
 
   private
 
-  def generate_and_set_auth_token
+  def generate_auth_token
     loop do
       self.auth_token = SecureRandom.hex
       break unless self.class.exists?(auth_token: auth_token)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:phone_number) }
     it 'validates presence of :auth_token' do
-      allow(user).to receive(:generate_and_set_auth_token)
+      allow(user).to receive(:generate_auth_token)
       expect(user).to validate_presence_of(:auth_token)
     end
   end
 
   describe 'before_validation(on: :create)' do
-    it 'generates and sets a new auth token' do
+    it 'generates a new auth token' do
       expect { user.save }.to change { user.auth_token }.from(nil).to(a_string_matching(/[0-9a-f]{32}/))
     end
   end
