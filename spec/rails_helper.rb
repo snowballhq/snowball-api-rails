f8f7cc58618d6894fb2ddfaa4b0b4dd549598646
James Martinez <jamescmartinez@gmail.com>
2014-10-23 21:42:06 -0700

    moved require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'paperclip/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'paperclip/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
