e2d2d383d69e5dd9c707ca4e6c28999a343a8e2a
James Martinez <jamescmartinez@gmail.com>
2014-10-24 08:31:30 -0700

    reel next clip method

--- app/models/reel.rb
+++ app/models/reel.rb
@@ -10,4 +10,12 @@ class Reel < ActiveRecord::Base
       [n.split.first].join(' ') unless n.nil?
     end.join(', ')
   end
+
+  def next_clip(user)
+    participation = Participation.where(user: user, reel: self).first
+    return nil unless participation
+    clip = participation.last_watched_clip
+    return clips.last(10).first unless clip
+    clips.where('created_at >= ? AND id != ?', clip.created_at, clip.id).order('created_at DESC').first
+  end
 end
--- spec/models/reel_spec.rb
+++ spec/models/reel_spec.rb
@@ -13,12 +13,40 @@ RSpec.describe Reel, type: :model do
 
   describe '#users_title' do
     it 'returns the first names of the last five users' do
-      reel = create(:reel)
+      reel.save!
       create_list(:participation, 6, reel: reel)
       users_title = reel.reload.users.last(5).map(&:name).map do |n|
         [n.split.first].join(' ') unless n.nil?
       end.join(', ')
       expect(reel.users_title).to eq(users_title)
+    end
+  end
+
+  describe '#next_clip(user)' do
+    context 'when clips exist in the reel' do
+      context 'when the user has previously watched a clip in this reel' do
+        it 'returns the next clip' do
+          reel.save!
+          clips = create_list(:clip, 2, reel: reel)
+          participation = create(:participation, reel: reel, last_watched_clip: clips.first)
+          expect(reel.next_clip(participation.user)).to eq(clips.last)
+        end
+      end
+      context 'when the user has never watched a clip in this reel' do
+        it 'returns the 10th most recent clip' do
+          reel.save!
+          clips = create_list(:clip, 11, reel: reel)
+          participation = create(:participation, reel: reel, last_watched_clip: nil)
+          expect(reel.next_clip(participation.user)).to eq(clips.second)
+        end
+      end
+    end
+    context 'when no clips exist in the reel' do
+      it 'returns nil' do
+        reel.save!
+        participation = create(:participation, reel: reel, last_watched_clip: nil)
+        expect(reel.next_clip(participation.user)).to be_nil
+      end
     end
   end
 end
