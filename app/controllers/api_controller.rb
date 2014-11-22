84f7f9d71536eecc07eaca8f50d9798d9a1ae291
James Martinez <jamescmartinez@gmail.com>
2014-11-22 10:48:39 -0800

    remove CSRF from API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
 
   class Snowball::InvalidPhoneNumberVerificationCode < StandardError
   end
