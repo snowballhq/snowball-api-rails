cae4f20d39e18f15e7df04508106993b50fe2c98
James Martinez <jamescmartinez@gmail.com>
2015-02-17 15:47:59 -0800

    Namespace api for subdomain api

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,10 @@ Rails.application.routes.draw do
   end
 
   constraints subdomain: 'api' do
-    concerns :api
+    namespace :api, path: nil do
+      concerns :api
+    end
   end
-
   namespace :api do
     concerns :api
   end
