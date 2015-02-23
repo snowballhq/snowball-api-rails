25fef2d996066692980279840646e3b46cd19221
James Martinez <jamescmartinez@gmail.com>
2015-02-22 23:42:39 -0800

    Staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,9 @@
+# Based off of production
+require Rails.root.join('config/environments/production')
+
+Bestly::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb and config/environments/production.rb.
+
+  # The default base URL to use when mailing users
+  config.action_mailer.default_url_options = { host: 'http://s.snowball.is' }
+end
