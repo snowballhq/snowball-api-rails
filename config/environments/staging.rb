5947c6d8c5b3443b204f2010cbebd929fd13b4ec
James Martinez <jamescmartinez@gmail.com>
2015-10-06 18:19:57 -0700

    Clean up configs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,4 @@ require Rails.root.join('config/environments/production')
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb and config/environments/production.rb.
-
-  # The default base URL to use when mailing users
-  config.action_mailer.default_url_options = { host: 'http://s.snowball.is' }
 end
