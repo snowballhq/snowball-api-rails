4122f1640fe6e9210191dc18450b308648d2454e
James Martinez <jamescmartinez@gmail.com>
2015-10-06 18:23:40 -0700

    Remove stray comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # namespace :v1, defaults: { format: :json } do
-    match 'users/sign-in', to: 'sessions#create', via: :post
-    match 'users/sign-up', to: 'registrations#create', via: :post
-    match 'users/phone-search', to: 'users#phone_search', via: :post
-    resources :users, only: [:index, :show, :update] do
-      match '/followers', to: 'follows#followers', via: :get
-      match '/following', to: 'follows#following', via: :get
-      match '/follow', to: 'follows#create', via: :post
-      match '/follow', to: 'follows#destroy', via: :delete
-    end
-    resources :clips, only: [:create, :destroy] do
-      resources :flags, only: :create
-      match '/likes', to: 'likes#create', via: :post
-      match '/likes', to: 'likes#destroy', via: :delete
-    end
-    match 'users/:user_id/clips/stream', to: 'clips#index', via: :get
-    match 'clips/stream', to: 'clips#index', via: :get
-  # end
+  match 'users/sign-in', to: 'sessions#create', via: :post
+  match 'users/sign-up', to: 'registrations#create', via: :post
+  match 'users/phone-search', to: 'users#phone_search', via: :post
+  resources :users, only: [:index, :show, :update] do
+    match '/followers', to: 'follows#followers', via: :get
+    match '/following', to: 'follows#following', via: :get
+    match '/follow', to: 'follows#create', via: :post
+    match '/follow', to: 'follows#destroy', via: :delete
+  end
+  resources :clips, only: [:create, :destroy] do
+    resources :flags, only: :create
+    match '/likes', to: 'likes#create', via: :post
+    match '/likes', to: 'likes#destroy', via: :delete
+  end
+  match 'users/:user_id/clips/stream', to: 'clips#index', via: :get
+  match 'clips/stream', to: 'clips#index', via: :get
 end
