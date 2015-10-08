133006b50831ee72afaf5b8c164c65b1394be3ed
James Martinez <jamescmartinez@gmail.com>
2015-10-08 15:29:24 -0700

    Specify username length to prevent CI from failing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username { Faker::Internet.user_name }
+    username { Faker::Internet.user_name(3..15) }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
     phone_number { Faker::Base.numerify('1415#3##47#') }
