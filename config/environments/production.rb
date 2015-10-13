12f681899745a957e19d255c2cf5fbb25ca32961
James Martinez <jamescmartinez@gmail.com>
2015-10-12 23:02:41 -0700

    Turn on Garbage Collection profiling for New Relic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,4 +30,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Turn on Garbage Collection profiling for New Relic
+  GC::Profiler.enable
 end
