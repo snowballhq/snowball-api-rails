a945d0946f261d4581b4e85a31cac0e5c9cacd95
James Martinez <jamescmartinez@gmail.com>
2016-03-04 09:55:57 -0800

    Use SNS for like push notification

--- app/models/like.rb
+++ app/models/like.rb
@@ -12,20 +12,6 @@ class Like < ActiveRecord::Base
   private
 
   def send_push_notification
-    headers = {
-      'X-Parse-Application-Id' => ENV['PARSE_APPLICATION_ID'],
-      'X-Parse-REST-API-Key' => ENV['PARSE_API_KEY'],
-      'Content-Type' => 'application/json'
-    }
-    message = "#{user.username} liked your clip. 👍"
-    body = {
-      where: {
-        user_id: clip.user.id
-      },
-      data: {
-        alert: message
-      }
-    }
-    HTTParty.post('https://api.parse.com/1/push', body: body.to_json, headers: headers, verify: false)
+    clip.user.send_push_notification("#{user.username} liked your clip. 👍")
   end
 end
